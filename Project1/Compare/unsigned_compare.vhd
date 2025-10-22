1d58787a106b8fdb41ffef9a4bc5578a77626491
andy <esliandy05@gmail.com>
2025-10-22 18:55:36 -0500

    signing of for tonight

---
+++ Project1/Compare/unsigned_compare.vhd
@@ -0,0 +1,27 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity UnsignedLessThan is
+    generic(
+        DATA_WIDTH : integer := 32
+    )
+    port(
+        i_A     : in std_logic_vector(DATA_WIDTH - 1 downto 0);
+        i_B     : in std_logic_vector(DATA_WIDTH - 1 downto 0);
+
+        o_AisLessThanB : out std_logic
+    );
+end UnsignedLessThan;
+
+architecture behaviour of UnsignedLessThan is
+
+    signal s_XOR_AB : std_logic_vector(DATA_WIDTH - 1 downto 0);
+
+begin
+
+    s_XOR_AB <= i_A XOR i_B;
+
+    
+
+end behaviour;
