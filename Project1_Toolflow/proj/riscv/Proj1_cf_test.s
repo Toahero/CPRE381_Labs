786a5d51550e81767d7b2e637faa77bf55aa4662
andy <esliandy05@gmail.com>
2025-11-02 23:12:57 -0600

    updated cf test

--- Project1_Toolflow/proj/riscv/Proj1_cf_test.s
+++ Project1_Toolflow/proj/riscv/Proj1_cf_test.s
@@ -1,63 +1,67 @@
 .text
 main:
-	li	s0,	1
-	li	s1,	2
-	li	s2,	3
-	li	s3,	4
-	li	s4,	5
-	li	s5,	6
-	li	s6,	7
-	li	s7,	8
-	li	s8,	9
-	li	s9,	10
-	li	s10,	11
-	li	s11,	12
+    # Needed to pass :(
+    li		sp,		0x7FFFEFFC
 
-	addi	sp,	sp,	0
+    li      s0 ,    1
+    li      s1 ,    2
+    li      s2 ,    3
+    li      s3 ,    4
+    li      s4 ,    5
+    li      s5 ,    6
+    li      s6 ,    7
+    li      s7 ,    8
+    li      s8 ,    9
+    li      s9 ,    10
+    li      s10,    11
+    li      s11,    12
 
-	sw	s0,	0(sp)
-	sw	s1,	4(sp)
-	sw	s2,	8(sp)
-	sw	s3,	12(sp)
-	sw	s4,	16(sp)
-	sw	s5,	20(sp)
-	sw	s6,	24(sp)
-	sw	s7,	28(sp)
-	sw	s8,	32(sp)
-	sw	s9,	36(sp)
-	sw	s10,	40(sp)
-	sw	s11,	44(sp)
+    addi    sp,     sp,     -48
+    sw      s0 ,    0 (sp)
+    sw      s1 ,    4 (sp)
+    sw      s2 ,    8 (sp)
+    sw      s3 ,    12(sp)
+    sw      s4 ,    16(sp)
+    sw      s5 ,    20(sp)
+    sw      s6 ,    24(sp)
+    sw      s7 ,    28(sp)
+    sw      s8 ,    32(sp)
+    sw      s9 ,    36(sp)
+    sw      s10,    40(sp)
+    sw      s11,    44(sp)
 
 
-	lw	s0,	0(sp)
-	lw	s1,	4(sp)
-	lw	s2,	8(sp)
-	lw	s3,	12(sp)
-	lw	s4,	16(sp)
-	lw	s5,	20(sp)
-	lw	s6,	24(sp)
-	lw	s7,	28(sp)
-	lw	s8,	32(sp)
-	lw	s9,	36(sp)
-	lw	s10,	40(sp)
-	lw	s11,	44(sp)
 
-	addi	s0, 	s0,	0
-	addi	s1,	s1,	0
-	addi	s2, 	s2,	0
-	addi	s3, 	s3,	0
-	addi	s4, 	s4,	0
-	addi	s5, 	s5,	0
-	addi	s6, 	s6,	0
-	addi	s7, 	s7,	0
-	addi	s8, 	s8,	0
-	addi	s9, 	s9,	0
-	addi	s10, 	s10,	0
-	addi	s11, 	s11,	0
+    lw      s0 ,    0 (sp)
+    lw      s1 ,    4 (sp)
+    lw      s2 ,    8 (sp)
+    lw      s3 ,    12(sp)
+    lw      s4 ,    16(sp)
+    lw      s5 ,    20(sp)
+    lw      s6 ,    24(sp)
+    lw      s7 ,    28(sp)
+    lw      s8 ,    32(sp)
+    lw      s9 ,    36(sp)
+    lw      s10,    40(sp)
+    lw      s11,    44(sp)
+    addi    sp,     sp,     48
 
-	wfi
-	
+    addi    s0 , 	s0 ,    0
+    addi    s1 ,	s1 ,    0
+    addi    s2 , 	s2 ,    0
+    addi    s3 , 	s3 ,    0
+    addi    s4 , 	s4 ,    0
+    addi    s5 , 	s5 ,    0
+    addi    s6 , 	s6 ,    0
+    addi    s7 , 	s7 ,    0
+    addi    s8 , 	s8 ,    0
+    addi    s9 , 	s9 ,    0
+    addi    s10, 	s10,    0
+    addi    s11, 	s11,    0
+    wfi	
+
 frame1:
+    addi    sp,     sp,     -16
 
 frame2:
 
