1f55df465d88e6a60577f4f978bfd224db52130c
Toahero <toahero@gmail.com>
2025-12-06 14:56:10 -0600

    Implemented Forwarding Module

--- Project2_Hardware/proj/src/TopLevel/Components/HazardTreatment/ForwardingUnit.vhd
+++ Project2_Hardware/proj/src/TopLevel/Components/HazardTreatment/ForwardingUnit.vhd
@@ -20,7 +20,6 @@ entity ForwardingUnit is
         i_MemInst            : in std_logic_vector(31 downto 0);
         i_WbInst             : in std_logic_vector(31 downto 0);
 
-        o_forwarding        : std_logic;
         o_ForwardSelRS1     : out std_logic_vector(1 downto 0);
         o_ForwardSelRS2     : out std_logic_vector(1 downto 0)
     );
