6cf0df14875726876a2741f44aba652ba4e0efdd
Toahero <toahero@gmail.com>
2025-12-15 15:20:54 -0600

    Modified Proj1_base_test for software nop insertion

--- Project2_Software/proj/riscv/Proj1_base_test.s
+++ Project2_Software/proj/riscv/Proj1_base_test.s
@@ -11,23 +11,38 @@ main:
 
 ### REGULAR ###
 	
+	
+	nop
+	nop
+	nop
 	# ADD
 	add	a2,	a0,	 a1
 	
+	
+	nop
+	nop
+	nop
 	# SUB
-	sub	a2,	a0,	a1
+	sub	a2,	a2,	a1
 	
 	# XOR
-	xor	a2,	a0,	a1
+	xor	a3,	a0,	a1
 	
+	
+	nop
+	nop
 	# OR
-	or	a2,	a0,	a1
+	or	a2,	a2,	a1
 	
+	nop
+	nop
+	nop
 	# AND
-	and	a2,	a0,	a1
+	and	a1,	a0,	a1
 	
+	nop
 	# SLL
-	sll	a2,	a0,	a0
+	sll	a2,	a0,	a3
 	
 	# SRL
 	srl	a2,	a0,	t0
@@ -35,38 +50,84 @@ main:
 	# SRA
 	sra	a2,	a1,	t0
 	
+	nop
+	nop
+	nop
 	# SLT
-	slt	a2,	a0,	a1
+	slt	a2,	a0,	a2
 	
 	# SLTU
 	sltu	a2,	a1,	t0
 
 ### IMMEDIATE ###
 	# XORI
-	xori	a2,	a0,	 15
+	nop
+	nop
+	nop
+	xori	t3,	t0,	 15
 	
 	# ORI
-	ori	a2,	a0,	 16
+	ori	t2,	a1,	 16
 	
 	# ANDI
-	andi	a2,	a0,	 17
+	andi	t0,	a0,	 17
 	
+	nop
 	# SLLI
-	slli	a2,	a0,	 3
+	slli	t2,	t3,	 3
 	
 	# SRLI
-	srli	a2,	a0,	 2
+	srli	a2,	t3,	 2
 	
+	nop
+	nop
 	# SRAI
-	srai	a2,	a1,	 2
+	srai	a2,	t2,	 2
 	
 	# SLTI
-	slti	a2,	a0,	 3
-	slti	a2,	a0,	 6
-	slti	a2,	a0,	-6
+	slti	a2,	t3,	 3
 	
 	# SLTIU
-	sltiu	a2,	a0,	 3
-	sltiu	a2,	a0,	 6
-	sltiu	a2,	a0,	-6	
+	sltiu	a0,	t0,	 3
+	
+	ori s0, x0, 0x123
+	nop
+	nop
+	nop
+	j skip
+	nop
+	li s0, 0xffffffff
+skip:
+	ori s1, x0, 0x123
+	
+	nop
+	nop
+	nop
+	beq s0, s1, skip2
+	
+	nop
+	nop
+	nop
+	li s0, 0xffffffff
+	nop
+skip2:
+
+	jal fun
+	nop
+	ori s3, x0, 0x123	
+	beq s0, x0, exit
+	nop
+
+	ori s4, x0, 0x123
+	nop
+	j exit
+	nop
+fun:
+	ori s2, x0, 0x123
+	nop
+
+	jr ra
+	nop
+exit:
 	wfi
+
